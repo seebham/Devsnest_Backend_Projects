ce58cc039dfab2d1a3b50c65a1f0c08d52ee2458
seebham <shubhamisdev@gmail.com>
2021-09-17 23:35:28 +0530

    Enhanced 🚀

--- Day_09/routes/index.js
+++ Day_09/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Staring Animals Server" });
+  res.render("index", { title: "👀 Staring Animals Server" });
 });
 
 module.exports = router;
