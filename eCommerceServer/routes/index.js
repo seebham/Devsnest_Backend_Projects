bf12123bad835e8566bf20d06f608fe62b01faee
seebham <shubhamisdev@gmail.com>
2021-09-28 17:18:51 +0530

    Day 11 Done 🥳

--- eCommerceServer/routes/index.js
+++ eCommerceServer/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
+var registerInitialChecks = require("../middlewares/registerChecks");
+
 var router = express.Router();
+var register = require("../controllers/register");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.post("/register", registerInitialChecks, register);
 
 module.exports = router;
