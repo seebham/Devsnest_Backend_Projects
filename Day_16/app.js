dd8e46e35bd04fe794eff1b5363b0b68069302f4
seebham <shubhamisdev@gmail.com>
2021-10-04 18:18:25 +0530

    Day 16 done 🥳

--- Day_16/app.js
+++ Day_16/app.js
@@ -10,6 +10,7 @@ var passport = require("passport");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var passportRouter = require("./routes/passport");
+var streamRouter = require("./routes/stream");
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/stream", require("./routes/stream"));
 
 app.use(
   session({
